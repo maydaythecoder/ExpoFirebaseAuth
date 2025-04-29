6a33b727f0e5d19b47520057e41c8680002562eb
mayday <maydaythecoder@gmail.com>
2025-04-29 15:52:50 -0400

    Add Babel and Metro configuration for React Native environment
    - Introduced babel.config.js to configure Babel presets and plugins, including support for environment variables via react-native-dotenv and module resolution.
    - Added metro.config.js to extend the default Metro configuration, allowing for additional source file extensions.
    - Updated package.json and package-lock.json to include new dependencies: babel-plugin-module-resolver and react-native-dotenv, along with their type definitions.
    - Created types/env.d.ts to define environment variable types for better TypeScript support.
    - Modified firebase.ts to utilize environment variables directly from react-native-dotenv for Firebase configuration.
    - Added a new asset (flame.png) to the project.
    - Ensured consistent casing in TypeScript file names and updated paths for module resolution.

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      ["module:react-native-dotenv", {
+        "moduleName": "react-native-dotenv",
+        "path": ".env",
+        "blacklist": null,
+        "whitelist": null,
+        "safe": false,
+        "allowUndefined": true
+      }],
+      ["module-resolver", {
+        "root": ["./"],
+        "alias": {
+          "@": "./"
+        }
+      }]
+    ],
+  };
+}; 
